b1b0a3f39229f2235cefa1d186749996416f0750
Maarten Terpstra <maarten.terpstra@knmi.nl>
2018-01-04 12:27:54 +0100

    Can send mail to interested parties

--- src/main/java/nl/knmi/geoweb/backend/admin/AdminServices.java
+++ src/main/java/nl/knmi/geoweb/backend/admin/AdminServices.java
@@ -1,12 +1,25 @@
 package nl.knmi.geoweb.backend.admin;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import javax.activation.DataSource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +30,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import nl.knmi.adaguc.tools.Debug;
 import nl.knmi.adaguc.tools.HTTPTools;
@@ -29,13 +44,54 @@ public class AdminServices {
 	AdminStore adminStore ;
 	TafSchemaStore tafSchemaStore;
 
-	
+	@Autowired
+	JavaMailSender emailSender;
+
 	public AdminServices (final AdminStore adminStore, final TafSchemaStore tafSchemaStore) throws IOException {
 		this.adminStore = adminStore;
 		this.tafSchemaStore = tafSchemaStore;
 	}
 	
-	
+	@RequestMapping(path="/receiveFeedback", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public void distributeFeedback(HttpServletRequest req, HttpServletResponse response, @RequestBody String payload) throws MessagingException, UnsupportedEncodingException {
+		ObjectMapper om = new ObjectMapper();
+		JsonNode json_payload = null;
+		JSONResponse jsonResponse = new JSONResponse(req);
+		try {
+			json_payload = om.readTree(payload);
+		} catch (IOException e) {
+			e.printStackTrace();
+			jsonResponse.setException("Failed to read payload", e);
+		}
+		
+		String version = json_payload.at("/config/version").asText();
+		String desc_short = json_payload.at("/descriptions/short").asText();
+		String desc_long = json_payload.at("/descriptions/long").asText();
+
+		String whom = json_payload.at("/descriptions/name").asText();
+		
+		String subject;
+		if (whom != null && whom.length() > 0) {
+			subject = "Geoweb " + version + " feedback van " + whom + ": " + desc_short;
+		} else { 
+			subject = "Geoweb " + version + " feedback: " + desc_short;
+		}
+		
+		String content = "Hallo Geowebbers,\n\nEr is feedback binnengekomen van " + (whom != null && whom.length() > 0 ? whom : "anonymous👤") + ".\nHet probleem was \"" + desc_long + "\".\n\nGroetjes,\nDe Geoweb feedback verzamlaar.";
+		MimeMessage message = emailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true);
+		helper.setSubject(subject);
+		helper.setText(content);
+		helper.setTo("geoweb-scrumteam@knmi.nl");
+		helper.setFrom("geoweb-feedback@knmi.nl");
+        helper.addAttachment("debug_info.json", new ByteArrayResource(json_payload.toString().getBytes(StandardCharsets.UTF_8.name())));
+        emailSender.send(message);
+		
+		try {
+			jsonResponse.print(response);
+		} catch (Exception e1) {
+		}
+	}
 
 	@RequestMapping(path="/create", method=RequestMethod.POST,	produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public void createConfigurationItem(HttpServletRequest req, HttpServletResponse response, @RequestBody String payload) throws JsonProcessingException {
