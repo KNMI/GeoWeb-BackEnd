3eb78539e925750334dee7e3e2c28bf10a57518e
Ernst de Vreede <ernstdevreede@xs4all.nl>
2018-02-20 13:04:58 +0000

    Moved sigmet classes to GeoWeb-Aviation-Messageconverter

--- src/main/java/nl/knmi/geoweb/backend/admin/AdminServices.java
+++ src/main/java/nl/knmi/geoweb/backend/admin/AdminServices.java
@@ -1,25 +1,20 @@
 package nl.knmi.geoweb.backend.admin;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-import javax.activation.DataSource;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
