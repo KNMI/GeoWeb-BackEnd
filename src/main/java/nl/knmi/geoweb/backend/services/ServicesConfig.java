13223b3c19a5028b953ebbba5862d924fb158436
Ernst de Vreede <ernstdevreede@xs4all.nl>
2018-06-27 09:36:01 +0000

    Define objectMapper beans

--- src/main/java/nl/knmi/geoweb/backend/services/ServicesConfig.java
+++ src/main/java/nl/knmi/geoweb/backend/services/ServicesConfig.java
@@ -4,6 +4,7 @@ import java.util.TimeZone;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,7 +20,7 @@ public class ServicesConfig {
 	//public static final String DATEFORMAT_ISO8601 = "yyyy-MM-dd'TT'HH:mm:ss'Y'";
 	@Bean("sigmetObjectMapper")
 	public static ObjectMapper getSigmetObjectMapperBean() {
-		Debug.println("Init SigmetObjectMapperBean");
+		Debug.println("Init SigmetObjectMapperBean (services)");
 		ObjectMapper om = new ObjectMapper();
 		om.registerModule(new JavaTimeModule());
 		om.setTimeZone(TimeZone.getTimeZone("UTC"));
@@ -30,5 +31,43 @@ public class ServicesConfig {
 		om.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
 		return om;
 
+	}
+	
+	@Bean("tafObjectMapper")
+	public static ObjectMapper getTafObjectMapperBean() {
+		Debug.println("Init TafObjectMapperBean (services)");
+		ObjectMapper om = new ObjectMapper();
+		om.registerModule(new JavaTimeModule());
+		om.setTimeZone(TimeZone.getTimeZone("UTC"));
+//		om.setDateFormat(new SimpleDateFormat(DATEFORMAT_ISO8601));
+		om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		om.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+		return om;
+	}
+	
+	@Bean("geoWebObjectMapper")
+	public static ObjectMapper getGeoWebObjectMapperBean() {
+		Debug.println("Init GeoWebObjectMapperBean (services)");
+		ObjectMapper om = new ObjectMapper();
+		om.registerModule(new JavaTimeModule());
+		om.setTimeZone(TimeZone.getTimeZone("UTC"));
+//		om.setDateFormat(new SimpleDateFormat(DATEFORMAT_ISO8601));
+		om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		om.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+		return om;
+	}
+	
+	@Bean("objectMapper")
+	@Primary
+	public static ObjectMapper getObjectMapperBean() {
+		Debug.println("Init ObjectMapperBean (services)");
+		ObjectMapper om = new ObjectMapper();
+		om.registerModule(new JavaTimeModule());
+		om.setTimeZone(TimeZone.getTimeZone("UTC"));		
+		return om;
 	}
 }
