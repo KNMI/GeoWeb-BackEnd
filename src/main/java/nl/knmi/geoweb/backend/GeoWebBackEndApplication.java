d182bd6a250296e3551b84e62494f0657f587d8f
Stijn van Drongelen <rhymoid@gmail.com>
2018-09-15 12:23:29 +0000

    Migrated to Spring Boot 2.0.5

--- src/main/java/nl/knmi/geoweb/backend/GeoWebBackEndApplication.java
+++ src/main/java/nl/knmi/geoweb/backend/GeoWebBackEndApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import nl.knmi.adaguc.tools.Debug;
