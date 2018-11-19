fb553df9e6fcd7a5d4525822a5eabbbdaf1f5b81
NickSchouten97 <niakster@ziggo.nl>
2018-11-19 15:47:49 +0000

    SSE verbeterd en calculateTrigger aangepast

--- src/main/java/nl/knmi/geoweb/backend/triggers/TriggerTest.java
+++ src/main/java/nl/knmi/geoweb/backend/triggers/TriggerTest.java
@@ -1,6 +1,5 @@
 package nl.knmi.geoweb.backend.triggers;
 
-import HTTPClient.HTTPResponse;
 import nl.knmi.adaguc.tools.Tools;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.monitor.FileAlterationListenerAdaptor;
@@ -13,29 +12,24 @@ import org.json.simple.parser.ParseException;
 import org.locationtech.jts.util.Debug;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import ucar.ma2.Array;
 import ucar.ma2.InvalidRangeException;
 import ucar.nc2.*;
 import ucar.nc2.dataset.NetcdfDataset;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.sql.rowset.serial.SerialException;
 import java.io.*;
 import java.io.FileWriter;
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/triggers")
@@ -47,7 +41,7 @@ public class TriggerTest extends HttpServlet {
     public static String activetriggerjsonpath = null;
     public static String triggerjsonpath = null;
     public static String activeTriggerPath = "/nobackup/users/schouten/Triggers/ActiveTriggers/";
-    public static String triggerPath = "/nobackup/users/schouten/Triggers";
+    public static String triggerPath = "/nobackup/users/schouten/Triggers/";
     private static Array
             station = null,
             data = null,
@@ -59,19 +53,30 @@ public class TriggerTest extends HttpServlet {
     private boolean changed = false;
     private static JSONArray locarray = null;
     private static JSONArray files = null;
+    private ExecutorService nonBlockingService = Executors
+            .newCachedThreadPool();
 
     @RequestMapping(path= "/triggercalculate", method= RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public static JSONObject calculateTrigger(@RequestBody String payload) throws IOException, InvalidRangeException, ParseException {
+    public static JSONArray calculateTrigger(@RequestBody String payload) throws IOException, InvalidRangeException, ParseException {
+
+        // Everytime new triggers are calculated the old ones get deleted
+        File folder = new File(triggerPath);
+        File[] listOfFiles = folder.listFiles();
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].getName().endsWith(".json")) {
+                listOfFiles[i].delete();
+            }
+        }
 
         org.json.JSONObject triggerInfo = new org.json.JSONObject(payload);
         System.out.println(payload);
 
-        File folder = new File(activeTriggerPath);
-        File[] listOfFiles = folder.listFiles();
-        files = new JSONArray();
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                files.add(listOfFiles[i].getName());
+        File activefolder = new File(activeTriggerPath);
+        File[] listOfActiveFiles = activefolder.listFiles();
+        JSONArray files = new JSONArray();
+        for (int i = 0; i < listOfActiveFiles.length; i++) {
+            if (listOfActiveFiles[i].isFile()) {
+                files.add(listOfActiveFiles[i].getName());
             }
         }
 
@@ -81,7 +86,7 @@ public class TriggerTest extends HttpServlet {
 
         JSONParser parser = new JSONParser();
 
-        JSONObject json = new JSONObject();
+        JSONArray triggerResults = new JSONArray();
 
         for(int i = 0; i < files.size(); i++) {
             JSONObject triggerFile = (JSONObject) parser.parse(new FileReader(activeTriggerPath + String.valueOf(files.get(i))));
@@ -122,20 +127,22 @@ public class TriggerTest extends HttpServlet {
             if (jsoncreated) {
                 // Setting a format of the date and time with only numbers (to put in the name of the trigger file)
 
-                DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-                        .appendValue(ChronoField.YEAR, 4)
-                        .appendValue(ChronoField.MONTH_OF_YEAR, 2)
-                        .appendValue(ChronoField.DAY_OF_MONTH, 2)
-                        .appendValue(ChronoField.HOUR_OF_DAY, 2)
-                        .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
-                        .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
-                        .appendValue(ChronoField.MILLI_OF_SECOND, 5)
-                        .toFormatter();
+//                DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+//                        .appendValue(ChronoField.YEAR, 4)
+//                        .appendValue(ChronoField.MONTH_OF_YEAR, 2)
+//                        .appendValue(ChronoField.DAY_OF_MONTH, 2)
+//                        .appendValue(ChronoField.HOUR_OF_DAY, 2)
+//                        .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+//                        .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+//                        .appendValue(ChronoField.MILLI_OF_SECOND, 5)
+//                        .toFormatter();
 
-                activetriggerjsonpath = triggerPath + "trigger_" + LocalDateTime.now().format(formatter) + ".json";  // Path + name where the trigger will be saved as a json file
+                activetriggerjsonpath = triggerPath + "trigger_" + UUID.randomUUID() + ".json";  // Path + name where the trigger will be saved as a json file
 
+                JSONObject json = new JSONObject();
                 json.put("locations", locarray);
-                json.put("phenomenon", triggerFile);
+                json.put("phenomenon", phen);
+                triggerResults.add(json);
 
                 // Creating the json file with a try catch
 
@@ -145,8 +152,11 @@ public class TriggerTest extends HttpServlet {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
             }
         }
+        return triggerResults;
+    }
 //        String path = triggerInfo.getString("serviceurl");
 //        Object par = phen.get("parameter");
 //        Object operator = phen.get("operator");
@@ -214,8 +224,6 @@ public class TriggerTest extends HttpServlet {
 //                e.printStackTrace();
 //            }
 //        }
-        return json;
-    }
 
     @RequestMapping(path= "/triggercreate", method= RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public static void addTrigger(@RequestBody String payload) throws IOException {
@@ -347,31 +355,68 @@ public class TriggerTest extends HttpServlet {
         return String.valueOf(triggerInfoList);
     }
 
-    @RequestMapping(path="/servlettest", produces=MediaType.TEXT_EVENT_STREAM_VALUE)
-    public void servletTest(HttpServletResponse response) throws IOException {
-        response.setContentType("text/event-stream");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter writer = response.getWriter();
+//    @RequestMapping(path="/servlettest", produces=MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public void servletTest(HttpServletResponse response) throws IOException {
+//        response.setContentType("text/event-stream");
+//        response.setCharacterEncoding("UTF-8");
+//        PrintWriter writer = response.getWriter();
+//
+//        File path = FileUtils.getFile("/nobackup/users/schouten/Triggers/ActiveTriggers");
+//        FileAlterationObserver observer = new FileAlterationObserver(path);
+//
+//        observer.addListener(new FileAlterationListenerAdaptor(){
+//
+//            @Override
+//            public void onFileCreate(File file) {
+//                changed = true;
+//            }
+//
+//        });
+//
+//        if(changed == true) {
+//            writer.write("data: A new file has been uploaded!\n\n");
+//            changed = false;
+//        }
+//
+//        FileAlterationMonitor monitor = new FileAlterationMonitor(500, observer);
+//
+//        try {
+//            monitor.start();
+//        } catch(IOException e) {
+//            System.out.println(e.getMessage());
+//        } catch(InterruptedException e) {
+//            System.out.println(e.getMessage());
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//        writer.close();
+//    }
 
+    @GetMapping("/sse")
+    public SseEmitter handleSse() {
         File path = FileUtils.getFile("/nobackup/users/schouten/Triggers/ActiveTriggers");
         FileAlterationObserver observer = new FileAlterationObserver(path);
+
+        SseEmitter emitter = new SseEmitter();
 
         observer.addListener(new FileAlterationListenerAdaptor(){
 
             @Override
             public void onFileCreate(File file) {
-                changed = true;
+                nonBlockingService.execute(() -> {
+                    try {
+                        emitter.send("A new file has been uploaded!");
+                        emitter.complete();
+                    } catch (Exception ex) {
+                        emitter.completeWithError(ex);
+                    }
+                });
             }
 
         });
 
-        if(changed == true) {
-            writer.write("data: A new file has been uploaded!\n\n");
-            changed = false;
-        }
-
         FileAlterationMonitor monitor = new FileAlterationMonitor(500, observer);
-
         try {
             monitor.start();
         } catch(IOException e) {
@@ -382,16 +427,16 @@ public class TriggerTest extends HttpServlet {
             System.out.println(e.getMessage());
         }
 
-        writer.close();
+        return emitter;
     }
 
-    private static void createJSONObject(int i){
+    private static void createJSONObject(int x){
         JSONObject locations = new JSONObject();
-        locations.put("lat", lat.getDouble(i));
-        locations.put("lon", lon.getDouble(i));
-        locations.put("name", station.getObject(i));
-        locations.put("code", code.getObject(i));
-        locations.put("value", data.getDouble(i));
+        locations.put("lat", lat.getDouble(x));
+        locations.put("lon", lon.getDouble(x));
+        locations.put("name", station.getObject(x));
+        locations.put("code", code.getObject(x));
+        locations.put("value", data.getDouble(x));
         locarray.add(locations);
     }
 
