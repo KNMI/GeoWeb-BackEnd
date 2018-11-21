3cad012a55c7c09dc9a5d7e7ea10f0c7336f4bcf
NickSchouten97 <niakster@ziggo.nl>
2018-11-21 10:46:08 +0000

    WebSocket test toegevoegd

---
+++ src/main/java/nl/knmi/geoweb/backend/triggers/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package nl.knmi.geoweb.backend.triggers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableAsync
+@EnableWebSocketMessageBroker
+@Controller
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chatWS").setAllowedOrigins("*").withSockJS();
+    }
+}
