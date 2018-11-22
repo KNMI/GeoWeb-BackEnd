2ff15707bfa21351c011e7bdd7fb82a017d119cd
NickSchouten97 <niakster@ziggo.nl>
2018-11-22 15:35:36 +0000

    WebSocket test werkend gekregen

--- src/main/java/nl/knmi/geoweb/backend/triggers/WebSocketConfig.java
+++ src/main/java/nl/knmi/geoweb/backend/triggers/WebSocketConfig.java
@@ -3,15 +3,13 @@ package nl.knmi.geoweb.backend.triggers;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
+@EnableScheduling
 @EnableAsync
 @EnableWebSocketMessageBroker
-@Controller
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
@@ -21,6 +19,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chatWS").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/websockettest").setAllowedOrigins("*").withSockJS();
     }
+
 }
